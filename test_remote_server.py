a9f27e35cdc0500e0626873fad7b0a896ff53364
Calder White <calderwhite1@gmail.com>
2025-11-02 19:48:01 -0500

    Add HTTPS test.

--- test_remote_server.py
+++ test_remote_server.py
@@ -9,17 +9,17 @@ import dotenv
 
 dotenv.load_dotenv('.test.env')
 
-def get_ip(proxies: dict[str, str] | None = None) -> str:
+def get_ip(proxies: dict[str, str] | None = None, protocol: str = "http") -> str:
     """Fetches the ip of the requester."""
     res = requests.request(
         "GET",
-        "http://ip.oxylabs.io/location",
+        f"{protocol}://ip.oxylabs.io/location",
         proxies=proxies,
     )
 
     return res.json()["ip"]
 
-def test_remote_server() -> None:
+def test_remote_server_http() -> None:
     """
     Ensures that when we make a request using the proxy, the ip identified by
     the host is different from our local ip.
@@ -28,5 +28,19 @@ def test_remote_server() -> None:
     remote_address = os.getenv("REMOTE_TEST_SERVER")
     local_ip = get_ip()
     remote_ip = get_ip(proxies={"http": remote_address, "https": remote_address})
+
+    assert remote_ip != local_ip, "The remote ip and local ip should not be the same. Ensure your .test.env is set correctly."
+
+def test_remote_server_https() -> None:
+    """
+    Ensures HTTPS is functional for the proxy servers.
+    """
+
+    remote_address = os.getenv("REMOTE_TEST_SERVER")
+    local_ip = get_ip(protocol="https")
+    remote_ip = get_ip(
+        proxies={"http": remote_address, "https": remote_address},
+        protocol="https"
+    )
 
     assert remote_ip != local_ip, "The remote ip and local ip should not be the same. Ensure your .test.env is set correctly."
